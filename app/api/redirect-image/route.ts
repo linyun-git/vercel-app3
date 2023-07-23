464b539ed9b5dcd3103aa36cfa832a6365dfec8d
ooame <ooame1@outlook.com>
2023-07-23 17:47:06 +0800

    feat: 重定向图片优化

--- app/api/redirect-image/route.ts
+++ app/api/redirect-image/route.ts
@@ -1,12 +1,31 @@
 import { NextResponse, NextRequest } from "next/server"
 
+const RANDOM_IMG = 'https://picsum.photos/200/300';
+const REDIRECTED = 'https://app3.suitesting.dev/api/redirect-image?redirect_count=1';
+
 export async function GET(req: NextRequest): Promise<NextResponse> {
 
     const { searchParams } = new URL(req.url);
+    const redirectCount = searchParams.get('redirect_count');
     const pos = searchParams.get('from');
     const mailId = searchParams.get('mail_id');
     const cssId = searchParams.get('css_id');
-    const imageId = Date.now() % 20;
+
+    if (!redirectCount) {
+        const imageId = Date.now() % 20;
+        console.log(`
+            redirect: true
+            pos: ${pos}
+            ${mailId ? `mid: ${mailId}` : ''}
+            ${cssId ? `cid: ${cssId}` : ''}
+            iid: ${imageId}
+            f-ip: ${req.headers.get('x-forwarded-for')}
+            ua: ${req.headers.get('user-agent')}
+        `);
+        return NextResponse.redirect(`${REDIRECTED}&from=${pos}&mail_id=${mailId}&image_id=${imageId}${cssId ? `&css_id=${cssId}` : ''}`);
+    }
+
+    const imageId = searchParams.get('image_id');
 
     console.log(`
         pos: ${pos}
@@ -17,5 +36,11 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
         ua: ${req.headers.get('user-agent')}
     `);
 
-    return NextResponse.redirect('https://app3.suitesting.dev/assets/1.jpg');
+    const res = await fetch(RANDOM_IMG);
+    const blob = await res.blob();
+    const headers = new Headers();
+    headers.set("Content-Type", res.headers.get('Content-Type'));
+    headers.set("Content-Disposition", res.headers.get('Content-Disposition'));
+    headers.set("Content-Length", res.headers.get('Content-Length'));
+    return new NextResponse(blob, { status: 200, statusText: "OK", headers });
 }
